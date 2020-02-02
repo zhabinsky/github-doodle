37c9cef14326398da353a08b27ecf164fbdcb5f7
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-02-02 05:47:48 +0200

    No message specified

--- src/createDoodle.js
+++ src/createDoodle.js
@@ -6,8 +6,72 @@ const moment = require ('moment');
 
 const dateFormat = 'ddd DD MMM YYYY h:mm:ss';
 const colors = ['#ffff', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
-const target = [2, 2];
+const target = [2, 7];
 // const target = [51, 7];
+
+const createDoodle = async file => {
+  const image = await instantiateImage (file);
+
+  // resize
+  await image.exec ('cover', ...target);
+  await image.exec ('greyscale');
+
+  const [pattern, dates] = await paintCanvas (image);
+
+  // generate .git project and write doodle commits
+  await generateGitProject (dates);
+
+  // show preview of the doodle
+  console.log (pattern);
+};
+
+module.exports = createDoodle;
+
+const generateGitProject = async dates => {
+  const dir = 'test_1';
+
+  const COMMANDS = {
+    MAKE_DIR: `mkdir -p ${dir} && rm -rf ${dir} && mkdir -p ${dir}`,
+    GIT_INIT: `git init`,
+    CREATE_FILE: `touch file.txt`,
+    COMMIT_ALL: 'git add . && git commit --date "{{date}} CET" -m \'Commit nr_{{msg}}\'',
+  };
+
+  const FAKE_COMMIT = (date, index) => {
+    const cmdFake = `
+    cd ${dir} 
+    echo "Commit nr_${index}" > file.txt && ${COMMANDS.COMMIT_ALL
+      .replace ('{{msg}}', index)
+      .replace (/{{date}}/g, date)}
+  `.trim ();
+    console.log (cmdFake);
+    return cmdFake;
+  };
+
+  const command = `
+    ${COMMANDS.MAKE_DIR}
+    cd ${dir}
+    ${COMMANDS.GIT_INIT}
+    ${COMMANDS.CREATE_FILE}
+  `;
+
+  const execResult = await shell (command.trim ());
+
+  const batchSize = 100;
+  const commitCommands = dates.map (FAKE_COMMIT);
+  for (let i = 0; i < commitCommands.length; i++) {
+    let commandCommits = '';
+    for (let j = 0; j < batchSize; j++) {
+      if (commitCommands[i + j]) commandCommits += commitCommands[i + j] + '\n';
+    }
+    await shell (commandCommits);
+    const percent = i / (commitCommands.length / batchSize);
+    console.log (Math.floor (percent * 100) + '%');
+    if (percent > 1) {
+      break;
+    }
+  }
+};
 
 const instantiateImage = async name => {
   return promisify (Jimp.read) (name)
@@ -107,17 +171,14 @@ async function paintCanvas (image) {
   return [pattern, dates];
 }
 
-const createDoodle = async file => {
-  const image = await instantiateImage (file);
-
-  // resize
-  await image.exec ('cover', ...target);
-  await image.exec ('greyscale');
-
-  const [pattern, dates] = await paintCanvas (image);
-
-  // show preview of the doodle
-  console.log (pattern);
-};
-
-module.exports = createDoodle;
+function shell (command) {
+  const exec = require ('child_process').exec;
+  return new Promise ((resolve, reject) => {
+    exec (command, (error, stdout, stderr) => {
+      if (error) {
+        console.warn (error);
+      }
+      resolve (stdout ? stdout : stderr);
+    });
+  });
+}
