d583566e06b8ebeb3313e503a639db17aec6c84d
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-02-02 04:46:49 +0200

    Generatign commit dates

--- src/createDoodle.js
+++ src/createDoodle.js
@@ -2,8 +2,12 @@ const convertToRGB = require ('hex-rgb');
 const chalk = require ('chalk');
 const Jimp = require ('jimp');
 const {promisify} = require ('util');
+const moment = require ('moment');
 
+const dateFormat = 'ddd DD MMM YYYY h:mm:ss';
 const colors = ['#ffff', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
+const target = [2, 2];
+// const target = [51, 7];
 
 const instantiateImage = async name => {
   return promisify (Jimp.read) (name)
@@ -23,8 +27,6 @@ const instantiateImage = async name => {
 const rgbColors = colors
   .map (convertToRGB)
   .map (({red, green, blue}) => [red, green, blue]);
-
-const target = [51, 7];
 
 async function getColorRange (image) {
   let dark = 0, light = 255;
@@ -59,8 +61,23 @@ const paintCell = (range, cells) => {
     bm[index + 1] = color[1];
     bm[index + 2] = color[2];
 
+    const dateStart = '31-12-2017';
+    let count = 0;
+
+    const getDate = () => {
+      count++;
+      return moment (dateStart, 'DD-MM-YYYY')
+        .add (x, 'weeks')
+        .add (y, 'days')
+        .hours (12)
+        .utcOffset (2, false)
+        .add (count, 'seconds')
+        .format (dateFormat);
+    };
+
     // save pixel cell
     cells.push ({
+      dates: new Array (colorIndex * 5).fill ().map (getDate),
       text: chalk.bgRgb (...color) (' ' + chalk.black (colorIndex)),
       index,
     });
@@ -77,8 +94,17 @@ async function paintCanvas (image) {
 
   // break line
   const br = ({text}, i) => text + ((i + 1) % target[0] === 0 ? '\n' : '');
+  const pattern = cells
+    .sort ((b, a) => b.index - a.index)
+    .map (br)
+    .join ('')
+    .trim ();
 
-  return cells.sort ((b, a) => b.index - a.index).map (br).join ('').trim ();
+  const dates = [];
+  cells.forEach (cell => dates.push (...cell.dates));
+  dates.sort ((a, b) => moment (a, dateFormat).diff (moment (b, dateFormat)));
+
+  return [pattern, dates];
 }
 
 const createDoodle = async file => {
@@ -86,10 +112,9 @@ const createDoodle = async file => {
 
   // resize
   await image.exec ('cover', ...target);
-
   await image.exec ('greyscale');
 
-  const pattern = await paintCanvas (image);
+  const [pattern, dates] = await paintCanvas (image);
 
   // show preview of the doodle
   console.log (pattern);
